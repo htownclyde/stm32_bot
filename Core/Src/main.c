f4a3ef9dbfea6b4cb032d3579e8c1a27d72a88fb
Clyde <htownclyde@gmail.com>
2023-09-04 01:03:12 -0500

    Improving CLI code structure.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -371,8 +371,6 @@ static void MX_TIM1_Init(void)
 
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   /* USER CODE BEGIN TIM1_Init 1 */
 
@@ -393,42 +391,15 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
-  {
-    Error_Handler();
-  }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
-  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
-  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
-  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-  sBreakDeadTimeConfig.DeadTime = 0;
-  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
-  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
-  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
-  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
-  HAL_TIM_MspPostInit(&htim1);
 
 }
 
@@ -448,7 +419,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
@@ -475,11 +446,20 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, led_r_Pin|led_g_Pin|led_b_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, led_r_Pin|led_g_Pin|led_b_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : led_r_Pin led_g_Pin led_b_Pin */
+  GPIO_InitStruct.Pin = led_r_Pin|led_g_Pin|led_b_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : xtilt_pwm_in_Pin ytilt_pwm_in_Pin */
   GPIO_InitStruct.Pin = xtilt_pwm_in_Pin|ytilt_pwm_in_Pin;
@@ -487,12 +467,15 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : led_r_Pin led_g_Pin led_b_Pin */
-  GPIO_InitStruct.Pin = led_r_Pin|led_g_Pin|led_b_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  /*Configure GPIO pin : UNNAMEDINT_Pin */
+  GPIO_InitStruct.Pin = UNNAMEDINT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(UNNAMEDINT_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
 
@@ -580,48 +563,63 @@ void StartConsoleTask(void *argument)
   /* USER CODE BEGIN StartConsoleTask */
 	// can use the msp ascii shifting checker to lowercase stuff
 	uint32_t buffer_size = 100;
-	uint8_t uart_buffer[buffer_size];
-	uint8_t uart_char[1];
-	uint8_t header[] = "--------------------\r\nRobotics Platform CLI\r\n--------------------\n\n";
-	uint8_t prompt[] = "\rEnter command: ";
-	uint8_t warning[] = "Invalid command.\n";
-	uint8_t i = 0;
+	uint32_t i = 0;
+	unsigned char uart_buffer[buffer_size];
+	unsigned char uart_char;
+	unsigned char header[] = "---------------------\r\nRobotics Platform CLI\r\n---------------------\n";
+	unsigned char prompt[] = "> ";
+	unsigned char warning[] = "Invalid command.";
+	char lower(char input){
+		if(input >= 'A' && input <= 'Z'){
+			input = input + 32;
+		}
+		return input;
+	}
 	void clear_buffers(){
-		uart_char[0] = 0x00;
+		uart_char = 0x00;
 		for(uint8_t j=0; j<=sizeof(uart_buffer); j++){
 			uart_buffer[j] = 0x00;
 		}
 	}
+	void print(unsigned char *input){
+		HAL_UART_Transmit(&huart1, input, sizeof(input), 500);
+	}
+	void println(unsigned char input[]){
+		print("\n\r");
+		print(input);
+	}
 	clear_buffers();
-	HAL_UART_Transmit(&huart1, header, sizeof(header), 500);
-	HAL_UART_Transmit(&huart1, prompt, sizeof(prompt), 500);
+	println(header);
+	println(prompt);
 	/* Infinite loop */
-	for(;;)
-	{
-		HAL_UART_Receive(&huart1, uart_char, 1, 500);
-		uart_buffer[i] = uart_char[0];
+	while(1){
 		osDelay(10);
-		if((uart_buffer[i] == '\r' && sizeof(uart_buffer)>1) || i == sizeof(uart_buffer)-2){
-			//for(uint8_t j=0; j<sizeof(uart_buffer); j++){
-				//if(*uart_buffer == 'echo\r'){
-				//	uart_buffer[i+1] = '\n';
-				//	HAL_UART_Transmit(&huart1, uart_buffer, sizeof(uart_buffer), 500);
-				//	uart_buffer[i+1] = '\n';
-				//	uart_buffer[i+2] = '\r';
-				//	HAL_UART_Transmit(&huart1, uart_buffer, sizeof(uart_buffer), 500);
-				//}
-				//else{
-					uart_buffer[i+1] = '\n';
-					uart_buffer[i+2] = '\r';
-					HAL_UART_Transmit(&huart1, uart_buffer, sizeof(uart_buffer), 500);
-					HAL_UART_Transmit(&huart1, warning, sizeof(warning), 500);
-				//}
-				clear_buffers();
-				i = 0;
-				HAL_UART_Transmit(&huart1, prompt, sizeof(prompt), 500);
+		HAL_UART_Receive(&huart1, &uart_char, 1, 500);
+		if(uart_char >= ' ' && uart_char <= '~'){
+			uart_buffer[i] = uart_char;
+			print(&uart_buffer[i]);
+			//if((uart_buffer[i] == '\r' && sizeof(uart_buffer)>1) || i == sizeof(uart_buffer)-2){
+			uart_char = 0x00;
+			i++;
 		}
-		i++;
-		osDelay(10);
+		else if(uart_char == '\r'){
+			switch(lower(uart_buffer[0])){
+				case 'h':
+					println("test");
+					break;
+				default:
+					println(warning);
+			}
+			println(prompt);
+			clear_buffers();
+			i = 0;
+		}
+		else if(i > 0 && (uart_char == '\b' || uart_char == 127)){
+			print("\b \b");
+			uart_char = 0x00;
+			uart_buffer[i] = 0x00;
+			i--;
+		}
 	}
   /* USER CODE END StartConsoleTask */
 }
